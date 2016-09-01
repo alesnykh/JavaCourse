b4812cb1e4758a2ed30e0b321e952fa970d6fb74
alesnykh <andriy.lesnykh@gmail.com>
2016-09-01 23:19:01 +0300

    Made changes in class Task1 regarding code review comments

--- src/module2/Task1.java
+++ src/module2/Task1.java
@@ -9,143 +9,175 @@ public class Task1 {
     static double[] myDoubleArray = {100500.00, 33.33333, 14.88, 22.401, 0.0000227, 1.0001, 13.444444, 12.1, -10.01, -122.1444};
 
     static int sum(int array[]) {
-        int s = 0;
-        for (int i = 0; i < array.length; i++) {
-            s = s + array[i];
+        int arraySum = 0;
+        for (int index = 0; index < array.length; index++) {
+            arraySum = arraySum + array[index];
         }
-        return s;
+        return arraySum;
     }
 
     static double sum(double array[]) {
-        double s = 0;
-        for (double i : array) s += i;
-        return s;
+        double arraySum = 0;
+        for (double i : array) arraySum += i;
+        return arraySum;
     }
 
     static int min(int array[]) {
-        int m = array[0], i = array.length - 1;
-        while (i >= 0) {
-            if (array[i] < m)
-                m = array[i];
-            i--;
+        int minInArray = array[0];
+        int index = array.length - 1;
+        while (index > 0) {
+            if (array[index] < minInArray) {
+                minInArray = array[index];
+            }
+            index--;
         }
-        return m;
+        return minInArray;
     }
 
     static double min(double array[]) {
-        int i = 0, last = array.length - 1;
-        double m = array[last];
-        while (i < array.length) {
-            if (m > array[i])
-                m = array[i];
-            i++;
+        int index = 1;
+        double minInArray = array[0];
+        while (index < array.length) {
+            if (minInArray > array[index]) {
+                minInArray = array[index];
+            }
+            index++;
         }
-        return m;
+        return minInArray;
     }
 
     static int max(int array[]) {
-        int i = 0;
-        int m = array[0];
-        while (i < array.length) {
-            if (m < array[i])
-                m = array[i];
-            i++;
+        int index = 1;
+        int maxInArray = array[0];
+        while (index < array.length) {
+            if (maxInArray < array[index]) {
+                maxInArray = array[index];
+            }
+            index++;
         }
-        return m;
+        return maxInArray;
     }
 
     static double max(double array[]) {
-        double m = array[0];
-        int i = 0;
-        while (i < array.length) {
-            if (array[i] > m)
-                m = array[i];
-            i++;
+        double maxInArray = array[0];
+        int index = 1;
+        while (index < array.length) {
+            if (array[index] > maxInArray) {
+                maxInArray = array[index];
+            }
+            index++;
         }
-        return m;
+        return maxInArray;
     }
 
     static int maxPositive(int array[]) {
-        int i = 0, mp = array[0];
-        while (i < array.length) {
-            if (array[i] > mp && mp >= 0)
-                mp = array[i];
-            i++;
+        int index = 1;
+        int maxPositiveInArray = array[0];
+        while (index < array.length) {
+            if (array[index] > maxPositiveInArray && array[index] >= 0) {
+                maxPositiveInArray = array[index];
+            }
+            index++;
         }
-        return mp;
+        return maxPositiveInArray;
     }
 
     static double maxPositive(double array[]) {
-        int i = 0;
-        double mp = array[0];
-        while (i < array.length) {
-            if (array[i] > mp && mp >= 0)
-                mp = array[i];
-            i++;
+        int index = 1;
+        double maxPositiveInArray = array[0];
+        while (index < array.length) {
+            if (array[index] > maxPositiveInArray && array[index] >= 0) {
+                maxPositiveInArray = array[index];
+            }
+            index++;
         }
-        return mp;
+        return maxPositiveInArray;
     }
 
     static int multiplication(int array[]) {
-        int i = 1, ml = array[0];
-        while (i < array.length) {
-            ml *= array[i];
-            i++;
+        int index = 1;
+        int arrayMultiplication = array[0];
+        while (index < array.length) {
+            arrayMultiplication *= array[index];
+            index++;
         }
-        return ml;
+        return arrayMultiplication;
     }
 
     static double multiplication(double array[]) {
-        int i = 1;
-        double ml = array[0];
-        while (i < array.length) {
-            ml *= array[i];
-            i++;
+        int index = 1;
+        double arrayMultiplication = array[0];
+        while (index < array.length) {
+            arrayMultiplication *= array[index];
+            index++;
         }
-        return ml;
+        return arrayMultiplication;
     }
 
     static int modulus(int array[]) {
-        int md = 0;
-        md = array[0] % array[array.length - 1];
-        return md;
+        int modFirstAndLast;
+        if (array[0] == 0 || array[array.length - 1] == 0){
+            modFirstAndLast = 0;
+        }
+        else {
+            modFirstAndLast = array[0] % array[array.length - 1];
+        }
+        return modFirstAndLast;
     }
 
     static double modulus(double array[]) {
-        double md = 0;
-        md = array[0] % array[array.length - 1];
-        return md;
+        double modFirstAndLast;
+        if (array[0] == 0 || array[array.length - 1] == 0) {
+            modFirstAndLast = 0;
+        }
+        else {
+            modFirstAndLast = array[0] % array[array.length - 1];
+        }
+        return modFirstAndLast;
     }
 
     static int secondLargest(int array[]){
-        for (int i = 0; i < array.length-1; i++){
-            int k = i, m;
-            for (int j = i+1; j < array.length; j++)
-                if (array[j] < array[k])
-                    k = j;
-            m = array[k];
-            array[k] = array[i];
-            array[i] = m;
+//      sorting array by ascending order
+        for (int mainIndex = 0; mainIndex < array.length-1; mainIndex++){
+            int intermediateIndex = mainIndex;
+            int maxElement;
+            for (int secondaryIndex = mainIndex+1; secondaryIndex < array.length; secondaryIndex++) {
+                if (array[secondaryIndex] < array[intermediateIndex]) {
+                    intermediateIndex = secondaryIndex;
+                }
+            }
+//          elements replacing block
+            maxElement = array[intermediateIndex];
+            array[intermediateIndex] = array[mainIndex];
+            array[mainIndex] = maxElement;
         }
+//      returns penultimate element of array
         return array[array.length-2];
     }
 
     static double secondLargest(double array[]){
-        int j = 0, i, k;
-        double m;
-        while (j < 2) {
-            m = array[j]; k = j;
-            for (i = j; i < array.length; i++)
-                if (m < array[i]) {
-                    m = array[i];
-                    k = i;
+//      partially sorting array by descending order
+        int mainIndex = 0;
+        int secondaryIndex;
+        int intermediateIndex;
+        double maxElement;
+        while (mainIndex < 2) {
+            maxElement = array[mainIndex];
+            intermediateIndex = mainIndex;
+            for (secondaryIndex = mainIndex; secondaryIndex < array.length; secondaryIndex++) {
+                if (maxElement < array[secondaryIndex]) {
+                    maxElement = array[secondaryIndex];
+                    intermediateIndex = secondaryIndex;
                 }
-            m = array[j];
-            array[j] = array[k];
-            array[k] = m;
-            j++;
+            }
+//          elements replacing block
+            maxElement = array[mainIndex];
+            array[mainIndex] = array[intermediateIndex];
+            array[intermediateIndex] = maxElement;
+            mainIndex++;
         }
-        return array[j-1];
+//      returns second element of array
+        return array[mainIndex-1];
     }
 
     public static void main(String[] args) {
